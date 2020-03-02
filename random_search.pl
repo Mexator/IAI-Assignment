5bb61ecd82c449c3ec2b9b6429bfccc99acd75cb
Anton Brisilin <brisilant@yandex.ru>
2020-03-02 22:00:52 +0300

    random_action function was introduced to select between passes and ordinary turns

--- random_search.pl
+++ random_search.pl
@@ -2,31 +2,16 @@
 
 random_search(Path):-
     start_pos(X,Y),
-    random_step(X,Y,[],Path).
+    random_action(X,Y,true,[],Path).
 
-random_step(X,Y,TurnsList,FinalPath):-
-    /*
-    * Here R is binded to action that agent will perform:
-    * 0  - go up (negative y)
-    * 1  - go right (positive x)
-    * 2  - go down (positive y)
-    * 3  - go left (negative x)
-    * 4  - pass up
-    * 5  - pass right
-    * 6  - pass down
-    * 7  - pass left
-    * 8  - pass up-right
-    * 9  - pass up-left
-    * 10 - pass down-right
-    * 11 - pass down-left
-    */
+random_action(X,Y,PassPossible,TurnsList,FinalPath):-
     (
         % Loose condition #1 (step at cell with orc)
         o(X,Y),
         append(TurnsList,[[X,Y]],NewTurnsList),
         length(TurnsList, Turn),
         format('Collision with orc at turn ~a\n', Turn),
-
+    
         FinalPath = NewTurnsList
     );
     (
@@ -37,45 +22,60 @@ random_step(X,Y,TurnsList,FinalPath):-
         format('Win in ~a turns\n', Turn),
 
         FinalPath = NewTurnsList
-    );
-    % Ordinary turn
-
-    random_between(0, 11, R),
+    );    
+    /*
+    * Here R is binded to action that agent will perform:
+    * 0  - pass in random direction
+    * 1  - move in random direction
+    */
+    (PassPossible -> random_between(0, 1, R)),
+        ((R=0) -> random_pass(X,Y,TurnsList,FinalPath)); 
+    random_step(X,Y,PassPossible,TurnsList,FinalPath).
+    
+random_pass(X,Y,TurnsList,FinalPath):-
+    /**
+    * Here R is binded to direction of pass that agent will perform.
+    */
+    random_between(0, 8, R),
     (
-        (R==0,  (NewY is Y-1, NewX is X  , Pass = -1));
-        (R==1,  (NewY is Y  , NewX is X+1, Pass = -1));
-        (R==2,  (NewY is Y+1, NewX is X  , Pass = -1));
-        (R==3,  (NewY is Y  , NewX is X-1, Pass = -1));
-        (R==4,  (NewY is Y  , NewX is X  , Pass =  up));
-        (R==5,  (NewY is Y  , NewX is X  , Pass =  right));
-        (R==6,  (NewY is Y  , NewX is X  , Pass =  down));
-        (R==7,  (NewY is Y  , NewX is X  , Pass =  left));
-        (R==8,  (NewY is Y  , NewX is X  , Pass =  up_right));
-        (R==9,  (NewY is Y  , NewX is X  , Pass =  up_left));
-        (R==10, (NewY is Y  , NewX is X  , Pass =  down_right));
-        (R==11, (NewY is Y  , NewX is X  , Pass =  down_left));
+        (R==1,  Pass =  up);
+        (R==2,  Pass =  right);
+        (R==3,  Pass =  down);
+        (R==4,  Pass =  left);
+        (R==5,  Pass =  up_right);
+        (R==6,  Pass =  up_left);
+        (R==7,  Pass =  down_right);
+        (R==8,  Pass =  down_left);
         fail
     ),
-    % If agent collides with the wall, retry the turn
+    (pass(X,Y,Pass,PassedX,PassedY),
     (
-        R < 4,
+        append(TurnsList,[[X,Y,"Pass"+Pass]],NewTurnsList),
+        random_action(PassedX,PassedY,false,NewTurnsList,FinalPath)
+    );
+    (
+        append(TurnsList,[[X,Y,"Pass"+Pass]],FinalPath),
+        length(TurnsList, Turn),
+        format('Bad pass at turn ~a\n', Turn)
+    )).  
+
+    random_step(X,Y,PassPossible,TurnsList,FinalPath):-
+        /*
+        * Here R is binded to move that agent will perform:
+        * 0 - go up (negative y)
+        * 1 - go right (positive x)
+        * 2 - go down (positive y)
+        * 3 - go left (negative x)
+        */
+        random_between(0, 3, R),
         (
-            append(TurnsList,[[X,Y]],NewTurnsList),
-            inBoundaries(NewX,NewY), 
-            random_step(NewX,NewY,NewTurnsList,FinalPath);
-            random_step(X,Y,TurnsList,FinalPath)
-        );
-        (
-            pass(NewX,NewY,Pass,PassedX,PassedY),
-            (
-                append(TurnsList,[[X,Y,"Pass",Pass]],NewTurnsList),
-                random_step(PassedX,PassedY,NewTurnsList,FinalPath)
-            );
-            (
-                append(TurnsList,[[X,Y,"Pass",Pass]],NewTurnsList),
-                length(TurnsList, Turn),
-                format('Bad pass at turn ~a\n', Turn),
-                FinalPath = NewTurnsList
-            )
-        )
-    ).  
+            R==0 -> (NewY is Y-1, NewX is X);
+            R==1 -> (NewY is Y, NewX is X+1);
+            R==2 -> (NewY is Y+1, NewX is X);
+            R==3 -> (NewY is Y, NewX is X-1)
+        ),
+        append(TurnsList,[[X,Y]],NewTurnsList),
+        % If agent collides with the wall, retry the turn
+        (inBoundaries(NewX,NewY) -> 
+        random_action(NewX,NewY,PassPossible,NewTurnsList,FinalPath);
+        random_step(X,Y,PassPossible,TurnsList,FinalPath)).
