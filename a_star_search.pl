1005a138866e2aae9a79174b95d2d4db3a194902
Anton Brisilin <brisilant@yandex.ru>
2020-03-07 19:34:14 +0300

    added template for A*

--- a_star_search.pl
+++ a_star_search.pl
@@ -3,7 +3,20 @@ a_star_search:-
     a_star_search(Path),
     format('Path found with A*: ~w\n',[Path]).
 a_star_search(FinalPath):-
-    start_a_star_search(0,0,[],[],[0,0],FinalPath).
+    start_a_star_search(0,0,[],[],[],FinalPath).
 start_a_star_search(X,Y,[],Turns,KnownCells,FinalPath):-
-    union([[X,Y]],KnownCells,NewKnownCells),
-    least_known().
+    visible(VisibleList),
+    union(VisibleList,KnownCells,NewKnownCells),
+    append([X,Y],Turns,NewTurns),
+    
+    (
+    t(TX,TY),
+    member([[TY,TY]], VisibleList),
+    TDCoords = [[TX,TY]],
+    start_a_star_search(X,Y,TDCoords,NewTurns,NewKnownCells,FinalPath)
+    );
+    % Searching most unknown space - Heuristics 1
+    false.
+start_a_star_search(X,Y,TDCoords,Turns,KnownCells,FinalPath):-
+    % Searching way to touchdown - Heuristics 2
+false.
